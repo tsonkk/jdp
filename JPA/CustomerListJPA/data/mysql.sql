c875c9d5d78701e3abe36194b6fd77530e007ee5
SonKK <tranghongson@gmail.com>
2026-02-10 14:41:28 +0700

    update JPA

--- JPA/CustomerListJPA/data/mysql.sql
+++ JPA/CustomerListJPA/data/mysql.sql
@@ -1,6 +1,5 @@
 -- Drop tables
 DROP TABLE IF EXISTS Customer;
-
 -- Create tables
 CREATE TABLE Customer(ID INT PRIMARY KEY AUTO_INCREMENT, Name TEXT, Age INT);
 INSERT INTO Customer(Name, Age) VALUES('Hồ Đình Khả', 50);
@@ -8,4 +7,4 @@ INSERT INTO Customer(Name, Age) VALUES('Lương An Vinh', 26);
 INSERT INTO Customer(Name, Age) VALUES('Lê Triệu Ngọc Đức', 40);
 INSERT INTO Customer(Name, Age) VALUES('Trần Văn Thắng', 52);
 INSERT INTO Customer(Name, Age) VALUES('Lê Thị Chung', 40);
-INSERT INTO Customer(Name, Age) VALUES('SonKK', 43);
+INSERT INTO Customer(Name, Age) VALUES('MySQL', 8);
